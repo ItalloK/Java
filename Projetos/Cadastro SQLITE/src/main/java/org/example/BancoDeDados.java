472ff8827a5d2032805783d18915de96488264b7
ItalloK <itallo016@gmail.com>
2024-10-04 09:26:51 -0300

    att

--- Projetos/Cadastro SQLITE/src/main/java/org/example/BancoDeDados.java
+++ Projetos/Cadastro SQLITE/src/main/java/org/example/BancoDeDados.java
@@ -10,6 +10,7 @@ public class BancoDeDados{
     public void VerificarConexao(){
         try (Connection conn = DriverManager.getConnection(bancoUrl)) {
             if (conn != null) {
+                //return true;
                 System.out.println("Conexão estabelecida com sucesso!");
             }
         } catch (SQLException e) {
@@ -17,32 +18,39 @@ public class BancoDeDados{
         }
     }
 
-    public void CadastrarUsuario(String nome){
-        String sql = "INSERT INTO Usuarios(nome) VALUES ('"+nome+"')";
-        try(Connection conn = DriverManager.getConnection(bancoUrl);
-            Statement stmt = conn.createStatement()){
-            stmt.executeUpdate(sql);
-            System.out.println("Usuario cadastrado com sucesso: " + nome);
-        }catch (SQLException e){
-            System.out.println(e.getMessage());
-        }
+    public void TodosUsuarios(){
+        String sql = "SELECT * FROM Usuarios";
     }
-    public void CadastrarUsuario(String nome, String email){
-        String sql = "INSERT INTO Usuarios(nome, email) VALUES ('"+nome+"', '"+email+"')";
-        try(Connection conn = DriverManager.getConnection(bancoUrl);
-            Statement stmt = conn.createStatement()){
-                stmt.executeUpdate(sql);
-                System.out.println("Usuario cadastrado com sucesso, Nome: " + nome + " Email: " + email);
-        }catch (SQLException e){
-            System.out.println(e.getMessage());
+
+    public void CadastrarUsuario(String... args) {
+        String nome = null, email = null, telefone = null, sql = null;
+
+        if (args.length > 0) {
+            nome = args[0];
+            sql = "INSERT INTO Usuarios(nome) VALUES ('"+nome+"')";
         }
-    }
-    public void CadastrarUsuario(String nome, String telefone, String email){
-        String sql = "INSERT INTO Usuarios(nome, telefone, email) VALUES('" + nome + "', '" + telefone + "', '" + email + "');";
+        if (args.length > 1) {
+            nome = args[0];
+            email = args[1];
+            sql = "INSERT INTO Usuarios(nome, email) VALUES ('"+nome+"', '"+email+"')";
+        }
+        if (args.length > 2) {
+            nome = args[0];
+            email = args[1];
+            telefone = args[2];
+            sql = "INSERT INTO Usuarios(nome, telefone, email) VALUES('" + nome + "', '" + telefone + "', '" + email + "');";
+        }
+
         try (Connection conn = DriverManager.getConnection(bancoUrl);
              Statement stmt = conn.createStatement()) {
-                stmt.executeUpdate(sql);
-                System.out.println("Usuario cadastrado com sucesso, Nome: " + nome + " Email: " + email + " telefone: " + telefone);
+            stmt.executeUpdate(sql);
+            if (args.length == 3) {
+                System.out.println("Usuário cadastrado com sucesso utilizando Nome: " + nome + ", Email: " + email + " e Telefone: " + telefone);
+            } else if (args.length == 2) {
+                System.out.println("Usuário cadastrado com sucesso utilizando Nome: " + nome + " e Email: " + email);
+            } else if (args.length == 1) {
+                System.out.println("Usuário cadastrado com sucesso utilizando Nome: " + nome);
+            }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
