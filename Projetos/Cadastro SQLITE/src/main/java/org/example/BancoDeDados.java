a348cb17ca4cafb1e3e2295df0626910993f84db
ItalloK <itallo016@gmail.com>
2024-10-04 18:45:19 -0300

    Att

--- Projetos/Cadastro SQLITE/src/main/java/org/example/BancoDeDados.java
+++ Projetos/Cadastro SQLITE/src/main/java/org/example/BancoDeDados.java
@@ -1,16 +1,16 @@
 package org.example;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class BancoDeDados{
     private String bancoUrl = "jdbc:sqlite:banco.db";
+    
     public void VerificarConexao(){
         try (Connection conn = DriverManager.getConnection(bancoUrl)) {
             if (conn != null) {
-                //return true;
                 System.out.println("Conexão estabelecida com sucesso!");
             }
         } catch (SQLException e) {
@@ -18,41 +18,56 @@ public class BancoDeDados{
         }
     }
 
-    public void TodosUsuarios(){
-        String sql = "SELECT * FROM Usuarios";
+    public void CadastrarUsuario(String nome, String email, String telefone) {
+        if(nome == "" || email == "" || telefone == ""){
+            System.out.println("Preencha todos os campos para cadastrar o Usuário.");
+            return;
+        }
+        String sql = "INSERT INTO Usuarios(nome, telefone, email) VALUES('" + nome + "', '" + telefone + "', '" + email + "');";
+        RealizarCadastro(sql);
     }
 
-    public void CadastrarUsuario(String... args) {
-        String nome = null, email = null, telefone = null, sql = null;
+    public void CadastrarMedico(String nome, String crm, String telefone, String email, String datanascimento){
+        if(nome == "" || crm ==  "" || telefone == "" || email == "" || datanascimento == ""){
+            System.out.println("Preencha todos os campos para cadastrar o Médico.");
+            return;
+        }
+        String sql = String.format("INSERT INTO Medicos (nome, crm, telefone, email, datanascimento) VALUES ('%s', '%s', '%s', '%s', '%s')", nome, crm, telefone, email, datanascimento);
+        RealizarCadastro(sql);
+    }
 
-        if (args.length > 0) {
-            nome = args[0];
-            sql = "INSERT INTO Usuarios(nome) VALUES ('"+nome+"')";
-        }
-        if (args.length > 1) {
-            nome = args[0];
-            email = args[1];
-            sql = "INSERT INTO Usuarios(nome, email) VALUES ('"+nome+"', '"+email+"')";
-        }
-        if (args.length > 2) {
-            nome = args[0];
-            email = args[1];
-            telefone = args[2];
-            sql = "INSERT INTO Usuarios(nome, telefone, email) VALUES('" + nome + "', '" + telefone + "', '" + email + "');";
-        }
-
+    private void RealizarCadastro(String sql){
         try (Connection conn = DriverManager.getConnection(bancoUrl);
              Statement stmt = conn.createStatement()) {
             stmt.executeUpdate(sql);
-            if (args.length == 3) {
-                System.out.println("Usuário cadastrado com sucesso utilizando Nome: " + nome + ", Email: " + email + " e Telefone: " + telefone);
-            } else if (args.length == 2) {
-                System.out.println("Usuário cadastrado com sucesso utilizando Nome: " + nome + " e Email: " + email);
-            } else if (args.length == 1) {
-                System.out.println("Usuário cadastrado com sucesso utilizando Nome: " + nome);
-            }
+            System.out.println("Cadastro Realizado");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
+
+    public List<Map<String, Object>> executeDQL(String query) {
+        List<Map<String, Object>> results = new ArrayList<>();
+        try (Connection conn = DriverManager.getConnection(bancoUrl)) {
+            if (conn != null) {
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(query);
+                while (rs.next()) {
+                    Map<String, Object> row = new HashMap<>();
+                    int columnCount = rs.getMetaData().getColumnCount();
+                    for (int i = 1; i <= columnCount; i++) {
+                        String columnName = rs.getMetaData().getColumnName(i);
+                        Object value = rs.getObject(i);
+                        row.put(columnName, value);
+                    }
+                    results.add(row);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return results;
+    }
+
+
 }
--- Projetos/Cadastro SQLITE/src/main/java/org/example/Main.java
+++ Projetos/Cadastro SQLITE/src/main/java/org/example/Main.java
@@ -1,16 +1,125 @@
 package org.example;
+import com.sun.source.tree.WhileLoopTree;
+
+import java.security.PublicKey;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        BancoDeDados bd = new BancoDeDados();
-        bd.VerificarConexao();
+    public static void main(String args[]) {
+        while(true) {
+            LimparTela();
+            String opcao = null;
+            Scanner scan = new Scanner(System.in);
+            int opt = -1;
+            System.out.println("Bem vindo ao Programa.");
+            System.out.println("1 - Para cadastrar um usuário.");
+            System.out.println("2 - Para cadastrar um Médico.");
+            System.out.println("3 - Para listar todos os Usuários");
+            System.out.println("4 - Para listar todos os Médicos");
+            System.out.print("Digite a opção desejada: ");
+            opt = scan.nextInt();
+            BancoDeDados bd = new BancoDeDados();
+            //scan.close();
+            switch (opt) {
+                case 1: {
+                    CadastrarUsuario(bd);
+                    break;
+                }
+                case 2: {
+                    CadastrarMedico(bd);
+                    break;
+                }
+                case 3: {
+                    ExibirUsuarios(bd);
+                    break;
+                }
+                case 4: {
+                    ExibirMedicos(bd);
+                    break;
+                }
+                default: {
+                    System.out.print("Opção Inválida.");
+                    System.exit(0);
+                }
+            }
+        }
+    }
 
-        bd.CadastrarUsuario("Italo", "+559899999-9999", "italo@teste.com.br");
-        bd.CadastrarUsuario("Gabriel", "italo@teste.com");
-        bd.CadastrarUsuario("Joaquim");
+    private static void CadastrarUsuario(BancoDeDados bd){
+        LimparTela();
+        Scanner scan = new Scanner(System.in);
+        String nome = null, email = null, telefone = null;
+        System.out.print("Digite o Nome do Usuário: ");
+        nome = scan.nextLine();
+        System.out.print("Digite o Email do Usuário: ");
+        email = scan.nextLine();
+        System.out.print("Digite o Telefone do Usuário: ");
+        telefone = scan.nextLine();
+        bd.CadastrarUsuario(nome, email, telefone);
+    }
+
+    private static void CadastrarMedico(BancoDeDados bd){
+        LimparTela();
+        Scanner scan = new Scanner(System.in);
+        String nome = null, crm = null, telefone = null, email = null, datanascimento = null;
+        System.out.print("Digite o Nome do Médico: ");
+        nome = scan.nextLine();
+        System.out.print("Digite o CRM do Médico: ");
+        crm = scan.nextLine();
+        System.out.print("Digite o Telefone do Médico: ");
+        telefone = scan.nextLine();
+        System.out.print("Digite o Email do Médico: ");
+        email = scan.nextLine();
+        System.out.print("Digite a Data de Nascimento do Médico: ");
+        datanascimento = scan.nextLine();
+        bd.CadastrarMedico(nome, crm, telefone, email, datanascimento);
+    }
+
+    private static void ExibirUsuarios(BancoDeDados bd){
+        LimparTela();
+        String query = "SELECT * FROM Usuarios";
+        List<Map<String, Object>> usuarios = bd.executeDQL(query);
+        for (Map<String, Object> usuario : usuarios) {
+            int id = (Integer) usuario.get("id");
+            String nome = (String) usuario.get("nome");
+            String email = (String) usuario.get("email");
+            String telefone = (String) usuario.get("telefone");
+            System.out.printf("ID: %d, Nome: %s, Email: %s e Telefone: %s\n", id, nome, email, telefone);
+        }
+        Pausar();
+    }
+
+    private static void ExibirMedicos(BancoDeDados bd){
+        LimparTela();
+        String query = "SELECT * FROM Medicos";
+        List<Map<String, Object>> usuarios = bd.executeDQL(query);
+        for (Map<String, Object> usuario : usuarios) {
+            int id = (Integer) usuario.get("id");
+            String nome = (String) usuario.get("nome");
+            String crm = (String) usuario.get("crm");
+            String telefone = (String) usuario.get("telefone");
+            String email = (String) usuario.get("email");
+            String datanascimento = (String) usuario.get("datanascimento");
+            System.out.printf("ID: %d, Nome: %s, CRM: %s, Telefone: %s, Emai: %s e Data de Nascimento %s\n", id, nome, crm, telefone, email, datanascimento);
+        }
+        Pausar();
+    }
+
+    private static void LimparTela(){
+        for(int i = 0; i<50; i++){
+            System.out.printf("\n");
+        }
+    }
+
+    private static void Pausar(){
+        System.out.printf("\nPressione Enter para continuar...");
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
     }
 }
 
